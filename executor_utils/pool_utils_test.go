c6e878368249229f86d5393c199f91f4308df8ac
jialiang.tjl <jialiang.tjl@alibaba-inc.com>
2022-07-25 11:45:12 +0800

    <ci>:(executor_utils):Add tests.

---
+++ executor_utils/pool_utils_test.go
@@ -0,0 +1,47 @@
+package executor_utils
+
+import (
+	"fmt"
+	"go_common/time_utils"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestNewPool(t *testing.T) {
+	pool, _ := NewPool(2)
+	tasks := make([]*Task, 10)
+
+	for j:=0; j<10; j++ {
+		i := j
+		go func() {
+			task := &Task{}
+			tasks[i] = task
+			task.Params = []interface{}{strconv.Itoa(i)}
+			task.Handler = func(v ...interface{}) {
+				for !task.Interupt {
+					fmt.Println(v[0].(string) + " is running...")
+					time_utils.WaitSeconds(1)
+				}
+			}
+			err := pool.Put(task)
+			if err != nil {
+				fmt.Printf("Fail to put a task.")
+			}
+		}()
+	}
+
+	closedNo := 0
+	for {
+		select {
+		case <- time.After(3 * time.Second):
+			tasks[closedNo].Interupt = true
+			fmt.Println("Close " + strconv.Itoa(closedNo))
+			closedNo += 1
+			if closedNo >= 10 {
+				return
+			}
+		}
+	}
+	pool.Close()
+}
